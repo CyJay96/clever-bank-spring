262ca0ff8f28278d528ef06ee4733c8a60d48502
CyJay96 <konstantin201586@gmail.com>
2023-08-26 15:18:15 +0300

    initial commit

---
+++ src/main/java/ru/clevertec/cleverbank/CleverBankApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.cleverbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleverBankApplication.class, args);
+    }
+
+}
